2cc0d8221c8cd52364d68b6fb401e9672fdb6bbe
Muhammed Shittu <devshittu@gmail.com>
2023-01-29 12:48:22 +0000

    App background set up.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-
 import { useState } from "react";
 import "./App.css";
+import AppBG from "./components/AppBG";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* uuser status */}
-      <div className="App bg-gray-800">
-      </div>
+      {/* user status */}
+      <div className="App bg-gray-800"></div>
+      <AppBG />
     </>
   );
 }
---
+++ src/components/AppBG.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AppBG() {
+  return (
+    <div className="flex h-screen w-full">
+      <div className="flex-1 bg-hero bg-cover bg-center bg-no-repeat"></div>
+    </div>
+  );
+}
+
+export default AppBG;
