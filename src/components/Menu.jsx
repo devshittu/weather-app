df55ca487f5d2ca8b757f4f8c12bd8b1cc8bb0ba
Muhammed Shittu <devshittu@gmail.com>
2023-01-30 21:35:48 +0000

    Implementing menu..

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Menu() {
   return (
-    <div>Menu</div>
+    <div className="relative overflow-auto h-auto pointer-events-auto z-20">
+      <div className="wrapper mt-[30vh] min-h-[80vh] p-20 pt-0 bg-slate-500/70">
+        Menu
+      </div>
+    </div>
   );
 }
 
