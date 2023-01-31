3fe7625cee58c2eba8024c1f1369122cec2ec80a
Muhammed Shittu <devshittu@gmail.com>
2023-01-31 09:29:35 +0000

    Implemented bg gradient for the app menu.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function Menu() {
   return (
     <div className="relative overflow-auto h-auto pointer-events-auto z-20">
-      <div className="wrapper mt-[30vh] min-h-[80vh] p-20 pt-0 bg-slate-500/70">
+      <div className="wrapper mt-[30vh] min-h-[80vh] p-20 pt-0 bg-gradient-to-t from-slate-900/70">
+        <div className="max-w-7xl mx-auto "></div>
         Menu
       </div>
     </div>
