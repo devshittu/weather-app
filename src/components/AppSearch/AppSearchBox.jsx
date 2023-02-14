004d5f35fe8279ab9b1e1674c94d4db0bcbf6700
Muhammed Shittu <devshittu@gmail.com>
2023-02-14 01:58:28 +0000

    Implemented search bar placeholder dark theme .

--- src/components/AppSearch/AppSearchBox.jsx
+++ src/components/AppSearch/AppSearchBox.jsx
@@ -8,15 +8,15 @@ function AppSearchBox() {
       <div className="md:w-4/6 z-20 relative">
         <div className="pt-2 relative   h-16 ">
           <input
-            className="  placeholder:text-slate-500 w-full h-full h-10x px-5 pr-16  text-lg md:text-2xl text-slate-700 dark:text-white/70
-          backdrop-blur bg-white/30 dark:bg-slate-900/30 border-2 md:border-4 border-white/80 dark:border-slate-900/80  rounded-xl md:rounded-lg shadow-md hover:bg-white/60 hover:shadow-lg focus:bg-white/60 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-white/80 active:shadow-lg transition duration-150 ease-in-out"
+            className="  placeholder:text-slate-500 dark:placeholder:text-white w-full h-full h-10x px-5 pr-16  text-lg md:text-2xl text-slate-700 dark:text-white/70
+          backdrop-blur bg-white/30 dark:bg-slate-900/30 border-2 md:border-4 border-white/80 dark:border-slate-900/80  rounded-xl md:rounded-lg shadow-md hover:bg-white/60 dark:hover:bg-slate-900/60 hover:shadow-lg focus:bg-white/60  dark:focus:bg-slate-900/60 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-white/80 dark:active:bg-slate-900/80 active:shadow-lg transition duration-150 ease-in-out"
             type="search"
             name="search"
             placeholder="Search a city..."
           />
           <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
             <svg
-              className="h-8 w-8 fill-white dark:fill-slate-800"
+              className="h-8 w-8 fill-white dark:fill-slate-400"
               version="1.1"
               id="Capa_1"
               x="0px"
--- src/components/AppSearch/SearchNoResultItem.jsx
+++ src/components/AppSearch/SearchNoResultItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SearchNoResultItem() {
   return (
-    <div className="w-full flex p-3 pl-4 h-24 items-center justify-center hover:bg-slate-500/40 cursor-pointer">
+    <div className="w-full flex p-3 pl-4 h-24 items-center justify-center  hover:bg-slate-500/40 dark:hover:bg-white/40 cursor-pointer">
       <div>
         <div className="font-bold text-lg md:text-2xl leading-loose">
           No result found for ""
