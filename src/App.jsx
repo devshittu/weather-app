d15854933dfe871bbf528949cc2c508d80f8a101
Muhammed Shittu <devshittu@gmail.com>
2023-03-04 11:56:59 +0000

    Updated the TimeWeatherInfo to get the temp from the globalState.

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import LogInPin from "./components/LogInPin";
 import Menu from "./components/Menu";
 import InfoFixedWrapper from "./components/InfoFixedWrapper";
 import LogInButtonWrapper from "./components/LogInButtonWrapper";
+import { useGlobalState } from "./GlobalState";
 function App() {
+  const [globalState, updateGlobalState] = useGlobalState();
   const [userLogInState, setUserLogInState] = useState("logged-out");
-
+console.log('globalState',globalState)
   const setUserStatus = (status) => {
     setUserLogInState(status);
   };
@@ -19,11 +21,18 @@ function App() {
       {/* <GlobalState> */}
         <div className={`App bg-gray-800 ${userLogInState}`}>
           <InfoFixedWrapper>
-            <TimeWeatherInfo />
+            <TimeWeatherInfo 
+              temperature={Math.round(
+                globalState?.city?.currentWeather?.main?.temp
+              )}/>
           </InfoFixedWrapper>
           <LogInPin />
           {/* Menu */}
           <Menu />
+          {/* left-[75%] md:left-[50%] */}
+          <div className="current-city absolute opacity-100 block pointer-events-nonex z-20 top-2 md:top-0 w-96 h-36 left-[75%] md:left-[50%]">
+           <h1> Your current city is detected as: {`${globalState?.currentCity?.cityName}, ${globalState?.currentCity?.countryName}`}</h1>
+          </div>
           <AppBG />
           <LogInButtonWrapper>
             <LogInButton setUserStatus={setUserStatus} />
