ff52a1803e7bf613e16856252e4c2f36d513eb2c
Muhammed Shittu <devshittu@gmail.com>
2023-03-06 01:17:57 +0000

    Applied built Placeholder for CityItem

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -295,7 +295,9 @@ function Menu({ setUserStatus }) {
             <div className="flex sm:-m-4 -mx-4 -mb-10 -mt-4 flex-wrap gap-4">
               {!memoizedState.nearbyCities ? (
                 <>
-                  <CityItemPlaceHolder className="from-sky-700/40" />
+                  <CityItemPlaceHolder photo="url" />
+                  <CityItemPlaceHolder photo="url" />
+                  <CityItemPlaceHolder photo="url" />
                 </>
               ) : (
                 memoizedState.nearbyCities?.map((item) => {
