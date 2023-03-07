c935aa19384bbf3176ecc46f9a2ad8fd3f483de5
Muhammed Shittu <devshittu@gmail.com>
2023-03-07 14:54:32 +0000

    Added custom functionality to the AppDialog class appwide.

--- src/components/AppWidgets/AppDialog.jsx
+++ src/components/AppWidgets/AppDialog.jsx
@@ -1,27 +1,65 @@
 import React, { useRef, useState, useEffect } from "react";
 import { useGlobalState } from "../../GlobalState";
 
-function AppDialog({ children }) {
+function AppDialog({
+  type = "info",
+  heading,
+  children,
+  closable,
+  onCancelCallback,
+  okCallback,
+}) {
   const [globalState, updateGlobalState] = useGlobalState();
   const [isVisible, setVisibleTo] = useState(false);
+  const [typeClassNames, setTypeClassNames] = useState(null);
   const dialogRef = useRef(null);
 
   const setVisibility = (value) => {
     setVisibleTo(value);
     updateGlobalState("modalVisibility", value);
-  }
+  };
   const onClose = () => {
     setVisibility(false);
     dialogRef.current.close();
+  };
+  const onCancel = () => {
+    //   "do something before finally canceling, probably a callback fxn"
+    if (onCancelCallback) onCancelCallback();
+    onClose();
   };
   const onShow = () => {
     setVisibility(true);
     dialogRef.current.showModal();
   };
+  const handleDialogType = () => {
+    let typeClass = "";
+    switch (type) {
+      case "success":
+        typeClass =
+          "bg-emerald-600 hover:bg-emerald-800 focus:ring-emerald-300 dark:focus:ring-emerald-800";
+        break;
+      case "warning":
+        typeClass =
+          "bg-amber-600 hover:bg-amber-800 focus:ring-amber-300 dark:focus:ring-amber-800";
+        break;
+      case "danger":
+        typeClass =
+          "bg-red-600 hover:bg-red-800 focus:ring-red-300 dark:focus:ring-red-800";
+        break;
+
+      default:
+        typeClass = 
+        "bg-blue-600 hover:bg-blue-800 focus:ring-blue-300 dark:focus:ring-blue-800";
+        break;
+    }
+    setTypeClassNames(typeClass);
+  };
+
   useEffect(() => {
     if (globalState.modalVisibility) {
       onShow();
     } else onClose();
+    handleDialogType();
   }, [globalState.modalVisibility]);
 
   return (
@@ -31,32 +69,35 @@ function AppDialog({ children }) {
         id="popup-modal"
         tabIndex="-1"
         className={`${
-          !isVisible ? "hidden" : 'block'
-        } fixed top-0 left-0 right-0 z-[50]  p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full`}
+          !isVisible ? "hidden" : "block"
+        } fixed top-0 left-0 right-0 z-[50]  p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full bg-transparent`}
       >
         <div className="relative w-full h-full max-w-md md:h-auto">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button
-              type="button"
-              className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-              data-modal-hide="popup-modal"
-            >
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+          <div className="relative bg-whitex  backdrop-blur bg-white/70 rounded-lg shadow dark:bg-gray-700">
+            {closable && (
+              <button
+                onClick={onClose}
+                type="button"
+                className="absolute  top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                data-modal-hide="popup-modal"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span className="sr-only">Close modal</span>
-            </button>
-            {children}
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 fill-red-600"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+            )}
+            {children ? children : (
             <div className="p-6 text-center">
               <svg
                 aria-hidden="true"
@@ -79,12 +120,12 @@ function AppDialog({ children }) {
               <button
                 data-modal-hide="popup-modal"
                 type="button"
-                className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                className={`${typeClassNames} text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2`}
               >
                 Yes, I'm sure
               </button>
               <button
-                onClick={onClose}
+                onClick={onCancel}
                 data-modal-hide="popup-modal"
                 type="button"
                 className="text-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:focus:ring-gray-600"
@@ -92,6 +133,7 @@ function AppDialog({ children }) {
                 No, cancel
               </button>
             </div>
+            )}
           </div>
         </div>
       </dialog>
