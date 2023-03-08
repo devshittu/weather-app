7ed2f63080f35217827613fa4c3c51e73cff82d1
Muhammed Shittu <devshittu@gmail.com>
2023-03-08 07:35:50 +0000

    Modified Appwide button.

--- src/components/AppWidgets/AppButton.jsx
+++ src/components/AppWidgets/AppButton.jsx
@@ -1,12 +1,83 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-function AppButton({ name, children, style, colorless, className, onClick }) {
+function AppButton({
+  children,
+  type,
+  size,
+  colorless,
+  className,
+  onClick,
+  disabled,
+}) {
+  const [typeClassNames, setTypeClassNames] = useState(null);
+  const [sizeClassNames, setSizeClassNames] = useState(null);
+  const handleType = () => {
+    let properties = "";
+    switch (type) {
+      case "success":
+        properties =
+          "text-emerald-600 hover:text-emerald-800 focus:ring-emerald-300/60 dark:focus:ring-emerald-800/60";
+        break;
+      case "warning":
+        properties =
+          "text-amber-600 hover:text-amber-800 focus:ring-amber-300/60 dark:focus:ring-amber-800/60";
+        break;
+      case "danger":
+        properties =
+          "text-red-600 hover:text-red-800 focus:ring-red-300/60 dark:focus:ring-red-800/60";
+        break;
+      case "info":
+        properties =
+          "text-blue-600 hover:text-blue-800 focus:ring-blue-300/60 dark:focus:ring-blue-800/60";
+          break;
+      default:
+        properties = "text-white focus:ring-white/60";
+        break;
+    }
+    setTypeClassNames(properties);
+  };
+  const handleSize = () => {
+    let properties = "";
+    switch (size) {
+      case "sm":
+        properties = "text-sm";
+        break;
+      case "md":
+        properties = "text-lg";
+        break;
+      case "lg":
+        properties = "text-2xl";
+        break;
+      default:
+        properties = "";
+        break;
+    }
+    setSizeClassNames(properties);
+  };
+
+  useEffect(() => {
+    handleType();
+    handleSize();
+  });
   return (
-    <button 
-    className={`inline-block px-3 py-1 md:px-6 md:py-2 text-lg md:text-2xl backdrop-blur bg-white/10 border-2 md:border-4 border-white/20 text-white rounded-md md:rounded-lg shadow-md hover:bg-white/30 hover:shadow-lg focus:bg-white/30 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-white/50 active:shadow-lg transition duration-150 ease-in-out${className ? ' ' + className : ''}`}
-    onClick={onClick}
+    <button
+      onClick={onClick}
+      className={`inline-block z-50
+      rounded-lg px-5 py-2.5 text-center
+      backdrop-blur focus:ring-4 focus:outline-none 
+      ${sizeClassNames}
+      ${typeClassNames}
+      ${
+        disabled
+          ? "text-slate-500 bg-slate-300/70 dark:bg-slate-600/70 cursor-not-allowed "
+          : "bg-white/10 hover:bg-white/30 dark:bg-slate-300/10 dark:hover:bg-slate-300/30" //default
+      } 
+
+      ${className}
+      `}
+      type="button"
+      disabled={disabled}
     >
-      {name}
       {children}
     </button>
   );
