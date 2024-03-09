7ac8e425395e864b51c4f26d9785efd90b33c902
Muhammed Shittu <devshittu@gmail.com>
2024-03-09 11:20:50 +0000

    Secured a safe place for theme switcher

--- src/components/InfoFixedWrapper.jsx
+++ src/components/InfoFixedWrapper.jsx
@@ -1,8 +1,13 @@
-function InfoFixedWrapper({ children }) {
+function InfoFixedWrapper({ children, rightChild }) {
   return (
-    <div className="flex content-end absolute opacity-0 left-0 bottom-0 m-10 ml-10 z-20 app-info-wrapper">
-      {children}
-    </div>
+    <>
+      <div className="flex content-end absolute opacity-0 left-0 bottom-0 m-10 ml-10 z-20 app-info-wrapper">
+        {children}
+      </div>
+      <div className="flex content-end absolute opacity-0 right-0 bottom-0 m-10 ml-10 z-20 app-info-wrapper">
+        {rightChild}
+      </div>
+    </>
   );
 }
 
