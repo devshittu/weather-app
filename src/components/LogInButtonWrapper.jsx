fc3cf287224234024d4e186c993be1da54a00aab
Muhammed Shittu <devshittu@gmail.com>
2024-03-09 11:20:15 +0000

    Removed unneeded codes

--- src/components/LogInButtonWrapper.jsx
+++ src/components/LogInButtonWrapper.jsx
@@ -2,17 +2,6 @@ import React from "react";
 
 function LogInButtonWrapper({ children }) {
   return (
-    // origin-[-50%_-40px]
-    // 
-// #app #sign-in-button-wrapper {
-//     position: absolute;
-//     bottom: 0px;
-//     pointer-events: none;
-//     left: 50%;
-//     opacity: 0;
-//     transform: translate(-50%, 40px);
-//     transition: opacity 250ms, transform 250ms;
-//     z-index: 2;
 
     <div className="absolute opacity-0 pointer-events-none bottom-2 md:bottom-0 left-[75%] md:left-[50%] z-20 login-button-wrapper ">
       {children}
